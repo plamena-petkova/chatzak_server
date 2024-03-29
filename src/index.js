4cf9a97aa54c41089abbb3cf9d8ea746efd2369f
Plamena Petkova <pl.petkova@gmail.com>
2024-03-29 21:24:41 +0200

    Remove static use

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ mongoose
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.json());
